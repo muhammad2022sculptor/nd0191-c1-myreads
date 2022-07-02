ad8c46f6b751f71f2b02130b7b6ec4ef702a0677
muhammad2022sculptor <muhammadshahiryarsculptor@gmail.com>
2022-07-02 16:48:18 +0500

    feat: was able to fetch the data from api.
    The books are available to be displayed in the respective sections.
    
    Commit 6

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,18 +1,36 @@
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import * as BookApi from "./BooksAPI";
 import SearchPage from "./SearchPage";
 import HomePage from "./HomePage";
 
 function App() {
+  const [allAvaiableBooks, setAllAvailableBooks] = useState();
+  const [currentBooks, setCurrentBooks] = useState();
+  const [toReadBooks, setToReadBooks] = useState();
+  const [readBooks, setReadBooks] = useState();
+  const [booksFetched, setBooksFetched] = useState(false);
+
+  // Api call to fetch the books in the database
+  const getAll = async () => {
+    let res = await BookApi.getAll();
+    setAllAvailableBooks(res);
+    setToReadBooks(res.filter((book) => book.shelf === "wantToRead"));
+    setCurrentBooks(res.filter((book) => book.shelf === "currentlyReading"));
+    setReadBooks(res.filter((book) => book.shelf === "read"));
+    setBooksFetched(true);
+  };
+
   useEffect(() => {
-    const getAll = async () => {
-      let res = await BookApi.getAll();
-      console.log(res);
-    };
-    getAll();
-  }, []);
+    if (!booksFetched) {
+      getAll();
+    }
+    console.log(allAvaiableBooks);
+    console.log(toReadBooks);
+    console.log(currentBooks);
+    console.log(readBooks);
+  }, [booksFetched]);
 
   return (
     <div className="app">
